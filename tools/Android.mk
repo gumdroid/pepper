9e906c4707efe8da5326d5f2f9a9e15a1fa2854b
Adam YH Lee <adam@gumstix.com>
2013-10-23 04:53:01 -0700

    Added sd card flashing tool

---
+++ tools/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+
+##################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := mkandroidsd
+LOCAL_SRC_FILES := mkandroidsd
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_IS_HOST_MODULE := true
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_PREBUILT)
