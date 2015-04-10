eabf9e2d8a46ce2525409c2092bf5abb24e5c16e
Ash Charles <ashcharles@gmail.com>
2015-04-10 15:12:54 -0700

    Update Pepper board configuration.
    Signed-off-by: Ash Charles <ashcharles@gmail.com>

--- device.mk
+++ device.mk
@@ -14,15 +14,12 @@ PRODUCT_COPY_FILES := \
 	$(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
 	$(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
 	$(LOCAL_PATH)/initlogo.rle:root/initlogo.rle \
-	$(LOCAL_PATH)/sd8787_uapsta.bin:system/etc/firmware/mrvl/sd8787_uapsta.bin \
+	$(LOCAL_PATH)/sd8787_uapsta.bin:system/vendor/firmware/mrvl/sd8787_uapsta.bin \
 	$(LOCAL_PATH)/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
 	$(LOCAL_PATH)/ti-tsc.idc:system/usr/idc/ti-tsc.idc \
 	frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
 	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
 	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
-
-# Don't make SGX work too hard
-# PRODUCT_PROPERTY_OVERRIDES := hwui.render_dirty_regions=false
 
 # Explicitly specify dpi, otherwise the icons don't show up correctly with SGX enabled
 # Our screen is 4.3" 480x272: roughly 120 display-independent-pixels
@@ -38,6 +35,4 @@ PRODUCT_PACKAGES += \
 	audio.primary.pepper
 
 # SGX
-#PRODUCT_PACKAGES += sgx_userspace_blobs
-
-$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
+PRODUCT_PACKAGES += sgx_userspace_blobs
--- pepper.mk
+++ pepper.mk
@@ -1,7 +1,6 @@
 # This is the build configuration for the Gumstix Pepper Single Board Computer.
 # It is based on the 'Toro' device conifguration file (Copyright Google Inc.)
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
 $(call inherit-product, device/gumstix/common/common.mk)
 $(call inherit-product, device/gumstix/pepper/device.mk)
 
